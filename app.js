bacc3fd782e7c4b2f45ffe02e2f6989761087e13
fizzatulaisha <fizzatulaisha@gmail.com>
2026-08-12 22:06:03 +0000

    Fix Vercel Express deployment

--- app.js
+++ app.js
@@ -92,6 +92,10 @@ app.get("/report", isLoggedIn, (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-});
+  });
+}
+
+module.exports = app;
